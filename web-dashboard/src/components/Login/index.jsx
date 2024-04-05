d4cafc40cd62b88acd9123530e46bc0fd2791599
Sourabh P <sourabhp94503@gmail.com>
2024-04-05 20:29:29 +0530

    Add: Login page

--- web-dashboard/src/components/Login/index.jsx
+++ web-dashboard/src/components/Login/index.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './index.css'
+
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+  const navigate = useNavigate()
+
+  const [userData, setUserData] = useState({
+    email: '',
+    mobileNumber: '',
+    password: '',
+    rePassword: ''
+  })
+
+  const [message, setMessage] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    navigate('/admindashboard')
+  }
+
   return (
-    <div>Login</div>
+    <div className='login-block'>
+      <div className="login-wrapper">
+        <div className="login-block1">
+          <h3 className="login-title">Login to your account</h3>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <input type="text" name='email' placeholder="Enter your email" required value={userData.email} onChange={(e) => setUserData({ ...userData, email: e.target.value })} />
+            <input type="password" name='password' placeholder="Enter your password" required value={userData.password} onChange={(e) => setUserData({ ...userData, password: e.target.value })} />
+            <p className='login-message'>{message}</p>
+            <input type="submit" value="Login to my account" />
+            <p>Not having an account? <Link className="login-link" to="/register">Go to Register</Link></p>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- web-dashboard/src/components/Register/index.jsx
+++ web-dashboard/src/components/Register/index.jsx
@@ -1,32 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
 
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
 
   const navigate = useNavigate()
 
+  const [userData, setUserData] = useState({
+    email: '',
+    mobileNumber: '',
+    password: '',
+    rePassword: ''
+  })
+
+  const [message, setMessage] = useState('')
+
   const onSubmit = (e) => {
     e.preventDefault()
 
+    if (userData.mobileNumber.length != 10) {
+      setMessage('Enter 10 digit mobile number')
+      return
+    }
+
+    if (userData.password !== userData.rePassword) {
+      setMessage('Both passwords must match')
+      return
+    }
 
     navigate('/admindashboard')
   }
 
   return (
     <div className='register-block'>
-      <div class="register-wrapper">
-        <div class="register-block1">
-          <h3 class="register-title">Create an account</h3>
-          <p>Create an account using the form below.</p>
+      <div className="register-wrapper">
+        <div className="register-block1">
+          <h3 className="register-title">Create an account</h3>
           <form onSubmit={(e) => onSubmit(e)}>
-            <input type="email" name='email' placeholder="Enter your email" required />
-            <input type="tel" name='mobileNumber' placeholder="Enter Your Mobile Number" required />
-            <input type="password" name='password' placeholder="Enter your password" required />
-            <input type="password" name='rePassword' placeholder="Re-Enter your password" required />
+            <input type="email" name='email' placeholder="Enter your email" required value={userData.email} onChange={(e) => setUserData({ ...userData, email: e.target.value })} />
+            <input type="number" name='mobileNumber' placeholder="Enter Your Mobile Number" required value={userData.mobileNumber} onChange={(e) => setUserData({ ...userData, mobileNumber: e.target.value })} />
+            <input type="password" name='password' placeholder="Enter your password" required value={userData.password} onChange={(e) => setUserData({ ...userData, password: e.target.value })} />
+            <input type="password" name='rePassword' placeholder="Re-Enter your password" required value={userData.rePassword} onChange={(e) => setUserData({ ...userData, rePassword: e.target.value })} />
+            <p className='register-message'>{message}</p>
             <input type="submit" value="Create my account" />
+            <p>Already having an account? <Link className="register-link" to="/login">Go to Login</Link></p>
           </form>
         </div>
       </div>
