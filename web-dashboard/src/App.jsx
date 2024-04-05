213cee9ac98bbc18e4b78bd62943b5fbb388b4a3
Sourabh P <sourabhp94503@gmail.com>
2024-04-05 19:29:46 +0530

    Add: Basic web-dashboard

---
+++ web-dashboard/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+
+import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
+
+function App() {
+
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+				</Routes>
+			</Router>
+		</div>
+	);
+}
+
+export default App;
---
+++ web-dashboard/src/components/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+const Home = () => {
+  return (
+    <>
+      <div id='home'>
+        <a href="/register" className='get-started-link'><button className='get-started'>Get Started</button></a>
+      </div>
+    </>
+  )
+}
+
+export default Home
